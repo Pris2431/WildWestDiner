4db6130f0e3cc172f4322ebc4bd75b1eb2cb490b
sha0yuan <167164575+sha0yuan@users.noreply.github.com>
2025-02-09 21:00:00 +0800

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,10 +1,8 @@
-const currentOrders = [
-    {
+const currentOrders = [{
         "id": "03e5b615-eccd-4aeb-97cd-52805b29f31a",
         "table": "Take-Away",
         "time": "2025-02-05T10:06:27Z",
-        "items": [
-            {
+        "items": [{
                 "name": "Beef Burger",
                 "quantity": 1,
                 "status": "waiting"
@@ -30,8 +28,7 @@ const currentOrders = [
         "id": "61d5216e-78e2-45b9-9f46-c7f712050adf",
         "table": "Take-Away",
         "time": "2025-02-05T10:02:04Z",
-        "items": [
-            {
+        "items": [{
                 "name": "Beef Burger",
                 "quantity": 1,
                 "status": "waiting"
@@ -57,8 +54,7 @@ const currentOrders = [
         "id": "3fcfe244-7913-4c03-bfae-0320de20bc43",
         "table": "Table 1",
         "time": "2025-02-05T10:20:10Z",
-        "items": [
-            {
+        "items": [{
                 "name": "Beef Burger",
                 "quantity": 1,
                 "status": "waiting"
@@ -99,8 +95,7 @@ const currentOrders = [
         "id": "bf722130-d9c3-4033-9bc5-ad2e830b91ee",
         "table": "Table 2",
         "time": "2025-02-05T10:20:40Z",
-        "items": [
-            {
+        "items": [{
                 "name": "Fish and Chips",
                 "quantity": 1,
                 "status": "waiting"
